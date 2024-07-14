9eb155d1ba374af8bdab9a407af6757168212dc3
Jilay Pandya <jilay.pandya@zeiss.com>
2024-07-14 16:24:36 +0200

    feat: assignment4 part1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,18 +2,34 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <time.h>
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
 //#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
+
 
 void* threadfunc(void* thread_param)
 {
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct timespec t={
+        .tv_nsec = thread_func_args->wait_to_obtain_ms*1000*1000,
+        .tv_sec = thread_func_args->wait_to_obtain_ms/1000
+
+    };
+    
+    nanosleep(&t,NULL);
+    pthread_mutex_lock(thread_func_args->mutex);
+    thread_func_args->thread_complete_success = true;
+    thread_param = thread_func_args;
+
+    t.tv_nsec = thread_func_args->wait_to_release_ms*1000*1000;
+    t.tv_sec = thread_func_args->wait_to_release_ms/1000;
+    nanosleep(&t,NULL);
+    pthread_mutex_unlock(thread_func_args->mutex);
     return thread_param;
 }
 
@@ -28,6 +44,18 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data *_thread_data = (struct thread_data*)malloc(sizeof(struct thread_data));
+    _thread_data->mutex = mutex;
+    _thread_data->wait_to_obtain_ms = wait_to_obtain_ms;
+    _thread_data->wait_to_release_ms = wait_to_release_ms;
+    int rc = pthread_create(thread,NULL,threadfunc,_thread_data);
+
+    bool status = true;
+    if(rc!=0){
+        printf("++++++++++++++++Thread creation failed");
+        status = false;
+        exit(EXIT_FAILURE);
+    }
+    return status;
 }
 
